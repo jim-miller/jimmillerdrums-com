e291582fbb9bde0dfa29d0dbc97efde989144467
Jim Miller <miller.jimd@gmail.com>
2026-01-11 23:17:16 -0500

    feat: allowing Tofu to update infrastructure from GHA

--- infra/iam.tf
+++ infra/iam.tf
@@ -7,11 +7,12 @@ resource "aws_iam_user" "github_actions" {
 # IAM policy for deployment permissions
 resource "aws_iam_policy" "github_actions_deployment" {
   name        = "GitHubActionsDeploymentPolicy"
-  description = "Policy for GitHub Actions to deploy website"
+  description = "Policy for GitHub Actions to deploy website and manage infra"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
+      # 1. Manage the Website Bucket content (Syncing files)
       {
         Effect = "Allow"
         Action = [
@@ -25,12 +26,37 @@ resource "aws_iam_policy" "github_actions_deployment" {
           "${aws_s3_bucket.website.arn}/*"
         ]
       },
+      # 2. Manage the Infrastructure settings (tofu apply)
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetBucket*",
+          "s3:PutBucket*",
+          "cloudfront:GetDistribution",
+          "cloudfront:UpdateDistribution",
+          "cloudfront:TagResource"
+        ]
+        Resource = "*" # Actions like 'ListDistributions' require "*"
+      },
+      # 3. Access the State Bucket (Hardcoded because it's external to this config)
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::jimmillerdrums-terraform-state",
+          "arn:aws:s3:::jimmillerdrums-terraform-state/*"
+        ]
+      },
+      # 4. Invalidation permissions
       {
         Effect = "Allow"
         Action = [
           "cloudfront:CreateInvalidation",
-          "cloudfront:GetInvalidation",
-          "cloudfront:ListInvalidations"
+          "cloudfront:GetInvalidation"
         ]
         Resource = "*"
       }
